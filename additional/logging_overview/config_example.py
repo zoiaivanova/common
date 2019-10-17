4432e5912461749cec0c9b544b518f558cd7e5e9
dvasilov <dvasilov@echoua.com>
2019-10-17 19:38:07 +0300

    Conf example

---
+++ additional/logging_overview/config_example.py
@@ -0,0 +1,10 @@
+import logging
+import logging.config
+
+logging.config.fileConfig(fname='log.conf', disable_existing_loggers=False)
+
+# Get the logger specified in the file
+logger = logging.getLogger(__name__)
+
+logger.debug('This is a debug message')
+logger.error('This is a error message')
